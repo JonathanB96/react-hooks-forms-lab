3d2b81f5bcb1fe7d07dbe5a1f49d0f184255305a
jonathan bola <bolajonathan@yahoo.com>
2022-03-17 16:48:18 -0500

    commit tree a67c508d124b4c8fccb4391a0a790fcf1908bdf0

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,6 +5,13 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [inputValue, setInputValue] = useState("")
+
+  function handleInputChange(e){
+    setInputValue(e.target.value.toUpperCase())
+    console.log(inputValue)
+
+  }
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
@@ -13,13 +20,13 @@ function ShoppingList({ items }) {
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
-    return item.category === selectedCategory;
+    return item.category === selectedCategory || item.name.startsWith(inputValue[1])
   });
 
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange ={handleInputChange} inputValue = {inputValue}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
